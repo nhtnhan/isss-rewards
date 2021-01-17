f90550239d77899f4f1ac4107ac99a3b281b5881
jha8 <42952573+jha8@users.noreply.github.com>
2021-01-16 20:24:42 -0700

    Add PointsView

---
+++ frontend/src/components/PointsView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PointsView = () =>{
+  return (
+    <div>
+        <h1>CURRENT POINTS</h1>
+        
+    </div>
+  )
+}
+
+export default PointsView;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import CustomNavbar from '../common/CustomNavbar'
+import CustomNavbar from '../common/CustomNavbar';
+import PointsView from '../components/PointsView';
 
-const HomePage = props => {
+const HomePage = (props) => {
   return (
     <div>
       <CustomNavbar></CustomNavbar>
+      <PointsView></PointsView>
     </div>
   )
 }
