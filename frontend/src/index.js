598e1b52a2775249cd5968921307102a4827238d
Nhan <nhnguyen@ualberta.ca>
2021-01-16 20:28:46 -0700

    pulled from joe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,30 +5,12 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Homepage from './pages/HomePage';
 import App from './App';
-<<<<<<< HEAD
-import HomePage from './pages/HomePage';
-// import LoginPage from './pages/Splash';
-
-import { loadState, saveState } from './localStorage';
-import Splash from './pages/Splash';
-
-// const persistdState = loadState();
-// const store = createStore(
-//   rootReducer,
-//   persistdState,
-//   applyMiddleware(thunk)
-// );
-// store.subscribe(()=>{
-//   saveState(store.getState());
-// })
-=======
 import Splash from './pages/Splash';
 import createStore from "./store/createStore";
 import history from './common/history';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore();
->>>>>>> bd247bb4273403889fbf2988a26192a3f9a157b4
 
 ReactDOM.render(
   <Provider store={store}>
