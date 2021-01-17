1658212844d64d16252eda9becbf4eda37b0c41b
pled2720 <d.pleskach@uleth.ca>
2021-01-16 22:38:38 -0700

    Stuck on the frontend

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -3,14 +3,20 @@ import FrontPage from '../images/front-page.jpg';
 import Logo from '../images/isss_logo.png';
 import '../styling/splash.css';
 import { Button,Link } from '@material-ui/core';
+import TC from './TermsAndConditions.jsx';
+
+
 
 const Splash = props => {
+    window.onload = () => TC.handleClickOpen();
+    
     return (
         <div>
+            <TC/>
             <div className="container">
                 <img src={Logo} className="logo" alt="The logo"/>
                 <img src={FrontPage} className="front-page" alt="This is us celebrating"/>
-                <Button variant="contained" color="primary" classname="login-button">
+                <Button variant="contained" color="primary" className="login-button">
                     <Link href="http://localhost:8000/accounts/google/login/?process=login" variant="body1">
                         Log In with CCID
                     </Link>
@@ -19,5 +25,8 @@ const Splash = props => {
         </div>
     )
 }
+
+
+  
 
 export default Splash;
---
+++ frontend/src/pages/TermsAndConditions.jsx
@@ -0,0 +1,86 @@
+import React, {useEffect} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  }
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+export default function CustomizedDialogs() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    console.log("Hello");
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return(
+    <div >
+      {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open dialog
+      </Button> */}
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Terms and Conditions
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+            lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
+            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
+            auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Accept
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
